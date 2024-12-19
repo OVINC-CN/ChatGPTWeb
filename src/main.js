ee5640e8379cfabd253c68d7ec325e17ed239e5d
orenzhang <orenzhang@ovinc.cn>
2024-12-19 14:02:36 +0800

    feat(md): support for katex render

--- src/main.js
+++ src/main.js
@@ -12,12 +12,14 @@ import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
+import createKatexPlugin from '@kangc/v-md-editor/lib/plugins/katex/cdn';
 
 import hljs from 'highlight.js';
 
 VMdPreview.use(githubTheme, {
   Hljs: hljs,
 });
+VMdPreview.use(createKatexPlugin());
 
 const app = createApp(App);
 
